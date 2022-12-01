cb7514ec4a7f4ac1af61376271266ea83f6b95b8
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2022-12-01 10:50:33 +0300

    Add Serilog

--- Program.cs
+++ Program.cs
@@ -3,10 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using ProductServiceWepApi.Data;
 using ProductServiceWepApi.DI;
 using ProductServiceWepApi.EndPoints;
+using Serilog;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+
+try
+{
 var builder = WebApplication.CreateBuilder(args);
 var sqlConBuilder = new SqlConnectionStringBuilder();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +22,9 @@ DI.CreateDI(builder.Services);
 sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
 
 
 var app = builder.Build();
@@ -26,10 +36,27 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.UseHttpsRedirection();//тоже middleware компонент
+app.UseHttpsRedirection();
 
-app.UseRouting();//сопоставление запросов с конкретными адресами (маршрутизация)
+app.UseSerilogRequestLogging();
+
+app.UseRouting();
 
 ProductEndPoints.AddEndpoints(app);
 
 app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
+
+
+
